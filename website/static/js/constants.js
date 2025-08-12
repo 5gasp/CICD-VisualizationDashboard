805ea7a56920a4a688056efb4649c8491e22aff7
Rafael Direito <rdireito@av.it.pt>
2025-08-12 22:50:11 +0100

    added logs and metrics for 5gasp's paper

--- website/static/js/constants.js
+++ website/static/js/constants.js
@@ -1,13 +1,20 @@
+/**
+ * @Author: Rafael Direito
+ * @Date:   2025-08-12 22:25:34
+ * @Last Modified by:   Rafael Direito
+ * @Last Modified time: 2025-08-12 22:41:10
+ */
 const rest_api = {
-	test_base_info: `${base_api}/gui/test-base-information`,
+    test_base_info: `${base_api}/gui/test-base-information`,
     test_stage_status: `${base_api}/gui/testing-process-status`,
     tests_performed: `${base_api}/gui/tests-performed`,
     console_log: `${base_api}/gui/test-console-log`,
-    test_files: `${base_api}/gui/test-output-file`
+    test_files: `${base_api}/gui/test-output-file`,
+    logs_and_metrics: `${base_api}/gui/logs-and-metrics`
 };
 
 let pages = {
-	test_info: "test-information.html",
+    test_info: "test-information.html",
     index: "index.html",
     console_log: "console-log.html",
     test_files: "test-files.html",
--- website/static/js/pages/test-information.js
+++ website/static/js/pages/test-information.js
@@ -1,3 +1,9 @@
+/**
+ * @Author: Rafael Direito
+ * @Date:   2025-08-12 22:25:34
+ * @Last Modified by:   Rafael Direito
+ * @Last Modified time: 2025-08-12 22:49:03
+ */
 // On Load, load CUs
 var test_id;
 var access_token;
@@ -5,21 +11,22 @@ window.onload = function () {
     checkLogin();
     test_id = findGetParameter("test_id");
     access_token = findGetParameter("access_token");
-    if(test_id == null) test_id = getCookie('test_id');
-    if(access_token == null) access_token = getCookie('access_token');
+    if (test_id == null) test_id = getCookie('test_id');
+    if (access_token == null) access_token = getCookie('access_token');
     get_test_base_info();
     get_test_process_stages();
     get_tests_performed();
+    get_test_logs_and_metrics();
 };
 
-function logout(){
+function logout() {
     deleteCookie("access_token");
     deleteCookie("test_id");
     // redirecting
     navigateTo(pages.index);
 }
 
-function refresh_results(){
+function refresh_results() {
     get_test_process_stages();
     get_tests_performed()
 }
@@ -32,50 +39,50 @@ let num_performed_tests = 0;
 
 function get_test_base_info() {
     $.ajax({
-			type: "GET",
-			url: rest_api.test_base_info,
-			data: {
-                "test_id": test_id,
-                "access_token": access_token,
-            },
-		})
-		.done(function (response) {
-            if (response.success){
+        type: "GET",
+        url: rest_api.test_base_info,
+        data: {
+            "test_id": test_id,
+            "access_token": access_token,
+        },
+    })
+        .done(function (response) {
+            if (response.success) {
                 $("#base_info_test_id").text(response.data.test_id);
                 $("#base_info_netapp_id").text(response.data.netapp_id);
                 $("#base_info_network_service_id").text(response.data.network_service_id);
                 $("#base_info_testbed").text(response.data.testbed_id);
                 $("#base_info_start_time").text(response.data.started_at.split(".")[0]);
-                if(response.data.test_status)
+                if (response.data.test_status)
                     $("#base_info_test_passed").show();
                 else
                     $("#base_info_test_failed").show();
                 return true;
             }
-            else{
+            else {
                 alert("Error!")
             }
-		})
-		.fail(function () {
-			alert("Error!")
-		});
+        })
+        .fail(function () {
+            alert("Error!")
+        });
 
-	return false;
+    return false;
 }
 
 
 function get_test_process_stages() {
     $.ajax({
-			type: "GET",
-			url: rest_api.test_stage_status,
-			data: {
-                "test_id": test_id,
-                "access_token": access_token,
-            },
-		})
-		.done(function (response) {
-            if (response.success){
-                if (response.data.length != num_test_process_stages){
+        type: "GET",
+        url: rest_api.test_stage_status,
+        data: {
+            "test_id": test_id,
+            "access_token": access_token,
+        },
+    })
+        .done(function (response) {
+            if (response.success) {
+                if (response.data.length != num_test_process_stages) {
                     $("#testing_process_status_tbody").empty();
                     for (let status of response.data) {
                         add_process_stage_row("#testing_process_status_tbody", status.timestamp, status.state, status.success)
@@ -84,34 +91,34 @@ function get_test_process_stages() {
                 }
                 return true;
             }
-            else{
+            else {
                 alert("Error!")
             }
-		})
-		.fail(function () {
-			alert("Error!")
-		});
+        })
+        .fail(function () {
+            alert("Error!")
+        });
 
-	return false;
+    return false;
 }
 
 
 function get_tests_performed() {
     $.ajax({
-			type: "GET",
-			url: rest_api.tests_performed,
-			data: {
-                "test_id": test_id,
-                "access_token": access_token,
-            },
-		})
-		.done(function (response) {
-            if (response.success){
-                if (response.data.length != num_performed_tests){
+        type: "GET",
+        url: rest_api.tests_performed,
+        data: {
+            "test_id": test_id,
+            "access_token": access_token,
+        },
+    })
+        .done(function (response) {
+            if (response.success) {
+                if (response.data.length != num_performed_tests) {
                     $("#tests_performed_tbody").empty();
 
-                    
-                    response.data.sort(function(a, b) {
+
+                    response.data.sort(function (a, b) {
                         var aID = parseInt(a.performed_test.split("-test-id-")[1]);
                         var bID = parseInt(b.performed_test.split("-test-id-")[1]);
                         console.log(aID, bID);
@@ -119,27 +126,27 @@ function get_tests_performed() {
                         if (aID < bID) return -1;
                         if (aID > bID) return 1;
                         return 0;
-                      });
+                    });
 
-                    for (let test_performed of response.data) { 
+                    for (let test_performed of response.data) {
                         add_test_performed_row("#tests_performed_tbody", test_performed.performed_test, test_performed.description, test_performed.start_time, test_performed.end_time, test_performed.success)
                     }
                     num_performed_tests = response.data.length
                 }
                 return true;
             }
-            else{
+            else {
                 alert("Error!")
             }
-		})
-		.fail(function () {
-			alert("Error!")
-		});
+        })
+        .fail(function () {
+            alert("Error!")
+        });
 
-	return false;
+    return false;
 }
 
-function add_process_stage_row(table_id, timestamp, stage_name, success){
+function add_process_stage_row(table_id, timestamp, stage_name, success) {
     let timestamp_splitted = timestamp.split("T");
     let date = timestamp_splitted[0]
     let time = timestamp_splitted[1].split(".")[0]
@@ -149,7 +156,7 @@ function add_process_stage_row(table_id, timestamp, stage_name, success){
         <td>${date}<br>${time}</td>
         <td>${stage_name}</td>
     `
-    if(success)
+    if (success)
         row += `
         <td class="td_success">Success</td>
         <td>No Observations</td>
@@ -157,31 +164,85 @@ function add_process_stage_row(table_id, timestamp, stage_name, success){
     else
         row += `<td class="td_fail">Fail</td>
         <td><a href="${pages.console_log}" target="_blank">Get Console Log</a></td>`;
-    
+
     row += '</tr>'
     $(table_id).append(row);
 }
 
-function add_test_performed_row(table_id, test_name, description, start, end, success){
+
+function get_test_logs_and_metrics() {
+    $.ajax({
+        type: "GET",
+        url: rest_api.logs_and_metrics,
+        data: {
+            "test_id": test_id
+        },
+    })
+        .done(function (response) {
+            if (response.success) {
+                if (response.data.length != num_test_process_stages) {
+                    $("#metrics_collected_tbody").empty();
+                    $("#logs_collected_tbody").empty();
+                    for (let metric of response.data["metrics"]) {
+                        let row = `<tr>
+                            <td class="col-8"> <a target="_blank"
+                            href="${metric.url}">
+                            ${metric.url}</a>
+                            </td>
+                            <td class="col-2">${metric.access_username}</td>
+                            <td class="col-2">${metric.access_password}</td>
+                        '</tr>'`;
+                        row += '</tr>'
+                        $("#metrics_collected_tbody").append(row);
+                    }
+                    for (let log of response.data["logs"]) {
+                        let row = `<tr>
+                            <td class="col-8"> <a target="_blank"
+                            href="${log.url}">
+                            ${log.url}</a>
+                            </td>
+                            <td class="col-2">${log.access_username}</td>
+                            <td class="col-2">${log.access_password}</td>
+                        '</tr>'`;
+                        row += '</tr>'
+                        $("#logs_collected_tbody").append(row);
+                    }
+                }
+                return true;
+            }
+            else {
+                alert("Error!")
+            }
+        })
+        .fail(function () {
+            alert("Error!")
+        });
+
+    return false;
+}
+
+
+
+function add_test_performed_row(table_id, test_name, description, start, end, success) {
 
     let id = test_name.split("-test-id-")[1]
     let test_name_simple = test_name.split("-test-id-")[0]
 
-    if (success == null){
+    if (success == null) {
         start = end = ["-", ""];
     }
-    else{
+    else {
         start = start.split(" ");
         end = end.split(" ");
     }
-    
+
     let row = `<tr>
         <td>${id}</td>
         <td>${test_name_simple}</td>
         <td>${start[0]}<br>${start[1]}</td>
         <td>${end[0]}<br>${end[1]}</td>
     `
-    if(success == null){
+    if (success == null) {
         row += `<td class="td_not_executed">Not Executed</td>`;
         row += `<td>${description}</td>`;
         row += `<td class="td_not_executed_soft">No Test Log</td>`;
@@ -189,16 +250,16 @@ function add_test_performed_row(table_id, test_name, description, start, end, su
     } else {
         if (success) {
             row += `<td class="td_success">Passed</td>`;
-        } else{
+        } else {
             row += `<td class="td_fail">Failed</td>`;
         }
         row += `<td>${description}</td>`;
         row += `<td><a href="${rest_api.test_files}?file_name=log.html&test_id=${test_id}&test_name=${test_name}&access_token=${access_token}" target="_blank">Test Log</a></td>`;
         row += `<td><a href="${rest_api.test_files}?file_name=report.html&test_id=${test_id}&test_name=${test_name}&access_token=${access_token}" target="_blank">Test Report</a></td>`;
-    
+
     }
 
-    
+
     row += '</tr>'
     $(table_id).append(row);
 }
@@ -206,7 +267,7 @@ function add_test_performed_row(table_id, test_name, description, start, end, su
 
 
 function toTitleCase(str) {
-    return str.replace(/\w\S*/g, function(txt){
+    return str.replace(/\w\S*/g, function (txt) {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
 }
